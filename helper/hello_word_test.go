93689b8afb44babba7dc1ca1344f9374cebebf07
Hanafi Adhi Prasetyo <hanafi.4148@students.amikom.ac.id>
2023-01-13 14:57:03 +0700

    Golang Unit Test

--- helper/hello_word_test.go
+++ helper/hello_word_test.go
@@ -2,9 +2,56 @@ package helper
 
 import (
 	"fmt"
-	"math/bits"
+	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestTableTest(t *testing.T) {
+	tests := []struct{
+		name string
+		request string
+		expected string
+	}{
+		{
+		name: "Hanafi",
+		request : "Hanafi",
+		expected : "Hello Hanafi",
+		},{
+		name: "Adhi",
+		request : "Adhi",
+		expected : "Hello Adhi",
+		},
+	}
+
+	for _,test := range tests{
+		t.Run(test.name,func(t *testing.T) {
+			result:= HelloWord(test.request)
+			require.Equal(t,test.expected,result)
+		})
+	}
+}
+
+
+func TestSubTest(t *testing.T) {
+	t.Run("Adhi",func(t *testing.T) {})
+	result:= HelloWord("Hanafi")
+	assert.Equal(t,"Hello Hanafi",result,"Result Must be 'Hello Hanafi'")
+	t.Run("hanafi",func(t *testing.T) {})
+	results:= HelloWord("Hanafi")
+	assert.Equal(t,"Hello Hanafi",results,"Result Must be 'Hello Hanafi'")
+}
+
+func TestMain(m *testing.M){
+	fmt.Println("Testing Before Unit Test")
+
+	m.Run()
+
+	fmt.Println("Testing After Unit TEst")
+
+}
 
 func TestHelloWord(t *testing.T) {
 	result := HelloWord("Adhi")
@@ -22,7 +69,26 @@ func TestHelloW(t *testing.T) {
 	fmt.Println("Hello Adhi Done")
 }
 
-func TestNum(t *testing.T) {
-	var data bits = bits(127)
-	fmt.Println(data)
+// testing with testify
+
+func TestHelloWordAssert(t *testing.T)  {
+	result:= HelloWord("Hanafi")
+	assert.Equal(t,"Hello Hanafi",result,"Result Must be 'Hello Hanafi'")
+	fmt.Println("Testing Selesai")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWord("Hanafi")
+	require.Equal(t,"Hello Hanafi", result, "Result must be 'Hello Hanafi'")
+}
+
+//testing skip
+
+func TestSkip(t *testing.T){
+	if runtime.GOOS == "windows" {
+		t.Skip("Can not run on Mac OS")
+	}
+
+	result := HelloWord("Hanafi")
+	assert.Equal(t, "Hello Hanafi",result, "Result must Be 'Hello Hanafi'")
 }
